6eb58f0762c5bb05fbff94cda3400d3fe971a8a0
Ranjeet Vishwakarma <rv238502@gmail.com>
2024-09-23 20:28:19 +0530

    Added componets folder with some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
-// import React from 'react'
-
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Base from "./components/Base";
+import Home from "./components/Home";
+import Order from "./components/Order";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [pizza, setPizza] = useState({ base: "", toppings: [] });
 
-export default App
+  const addBase = (base) => {
+    setPizza({
+      ...pizza,
+      base,
+    });
+  };
+
+  const addTopping = (topping) => {
+    let newToppings;
+    if (!pizza.toppings.includes(topping))
+      newToppings = [...pizza.toppings, topping];
+    else newToppings = pizza.toppings.filter((item) => item !== topping);
+    setPizza({
+      ...pizza,
+      toppings: newToppings,
+    });
+  };
+  return;
+  <>
+    <Header />
+
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+
+      <Route path="/base">
+        <Base addBase={addBase} pizza={pizza} />
+      </Route>
+
+      <Route path="/base">
+        <Base addBase={addTopping} pizza={pizza} />
+      </Route>
+
+      <Route path="/order">
+        <Order pizza={pizza} />
+      </Route>
+    </Switch>
+  </>;
+};
+
+export default App;
